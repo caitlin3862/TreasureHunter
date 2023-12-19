b47c3a791a4e70f33010dd0de0190d44b40bf5a5
BT_2N2_18 <sample@bths.edu>
2023-12-19 13:03:03 -0500

    Finished TH-1

--- src/Colors.java
+++ src/Colors.java
@@ -2,7 +2,7 @@ public class Colors {
 
     public static final String RESET = "\033[0m";      // Reset
 
-    public static final String BLACK = "\033[0;30m";   // BLACK
+    //public static final String BLACK = "\033[0;30m";   // BLACK
 
     public static final String RED = "\033[0;31m";     // RED
 
