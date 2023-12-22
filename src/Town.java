6335c0cec5436e9978d5d288b584616083dc1f2d
BT_2N2_18 <sample@bths.edu>
2023-12-22 14:02:33 -0500

    finished TH-10

--- src/Town.java
+++ src/Town.java
@@ -109,20 +109,23 @@ public class Town {
             goldDiff = (int) (Math.random() * 10) + 1;
             if (Math.random() > noTroubleChance) {
                 if (hunter.hasItemInKit("sword")) {
+                    printMessage = "";
                     printMessage += "The brawler, seeing your sword, realizes he picked a losing fight and gives you his gold.";
                     hunter.changeGold(goldDiff);
+                } else {
+                    printMessage += "Okay, stranger! You proved yer mettle. Here, take my gold." + Colors.RESET;
+                    printMessage += "\nYou " + Colors.GREEN + "won" + Colors.RESET + " the brawl and receive " + Colors.YELLOW + goldDiff + " gold" + Colors.RESET + ".";
+                    hunter.changeGold(goldDiff);
                 }
-                printMessage += "Okay, stranger! You proved yer mettle. Here, take my gold." + Colors.RESET;
-                printMessage += "\nYou " + Colors.GREEN + "won" + Colors.RESET + " the brawl and receive " + Colors.YELLOW + goldDiff + " gold" + Colors.RESET + ".";
-                hunter.changeGold(goldDiff);
             } else {
                 if (hunter.hasItemInKit("sword")) {
-                    printMessage += "The brawler, seeing your sword, realizes he picked a losing fight and gives you his gold.";
+                    printMessage = "";
+                    printMessage += "The brawler, seeing your sword, realizes he picked a losing fight and gives you his gold." + "\nYou receive " + Colors.YELLOW + goldDiff + " gold" + Colors.RESET + ".";
                     hunter.changeGold(goldDiff);
+                } else {
+                    printMessage += "\nYou " + Colors.GREEN + "won" + Colors.RESET + " the brawl and receive " + Colors.YELLOW + goldDiff + " gold" + Colors.RESET + ".";
+                    hunter.changeGold(-goldDiff);
                 }
-                printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!" + Colors.RESET;
-                printMessage += "\nYou lost " + Colors.YELLOW + goldDiff + " gold" + Colors.RESET + ".";
-                hunter.changeGold(-goldDiff);
 
             }
         }
